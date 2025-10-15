3207aa6875533a55ed0597c05372dd335ff69845
Vasiliki <vasilikihasapi@gmail.com>
2025-10-15 13:23:31 +0300

    Update config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+# Rythmiseis gia to WiFi kai to MQTT
+WIFI_SSID = "YourWiFiSSID"
+WIFI_PASSWORD = "YourWiFiPassword"
+MQTT_BROKER = "broker.hivemq.com"  # Dimosio broker gia dokimes
+MQTT_PORT = 8883                   # TLS port
+MQTT_TOPIC = "iot/secure_light"
+# Kleidi gia kryptografisi (16 bytes gia AES/Fernet)
+SECRET_KEY = b"1234567890abcdef"
+# Credentials gia aplo elegxo prosvasis
+USERNAME = "admin"
+PASSWORD = "2323"
